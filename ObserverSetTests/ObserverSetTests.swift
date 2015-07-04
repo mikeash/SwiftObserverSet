0e6ef4ebf03ffa927e7e93d8740f2feb6d131fe8
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-07-04 09:27:21 -0700

    added iOS Framework target. Verified Mac and iOS Tests all pass. Shared schemes.

--- ObserverSetTests/ObserverSetTests.swift
+++ ObserverSetTests/ObserverSetTests.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Mike Ash. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 import XCTest
 
 import ObserverSet
---
+++ ObserverSet_iOS/ObserverSet_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ObserverSet_iOS.h
+//  ObserverSet_iOS
+//
+//  Created by Jeffrey Bergier on 7/4/15.
+//  Copyright (c) 2015 Mike Ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObserverSet_iOS.
+FOUNDATION_EXPORT double ObserverSet_iOSVersionNumber;
+
+//! Project version string for ObserverSet_iOS.
+FOUNDATION_EXPORT const unsigned char ObserverSet_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObserverSet_iOS/PublicHeader.h>
+
+
