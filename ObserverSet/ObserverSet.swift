00e362700d37e1eb733d4e27c4d55ada1c8578c4
Kane Cheshire <kane.cheshire@theappbusiness.com>
2016-10-21 09:53:48 +0100

    Adds `@disardableResult`

--- ObserverSet/ObserverSet.swift
+++ ObserverSet/ObserverSet.swift
@@ -33,7 +33,8 @@ open class ObserverSet<Parameters>: CustomStringConvertible {
     fileprivate var entries: [ObserverSetEntry<Parameters>] = []
     
     public init() {}
-    
+  
+  @discardableResult
     open func add<T: AnyObject>(_ object: T, _ f: @escaping (T) -> (Parameters) -> Void) -> ObserverSetEntry<Parameters> {
         let entry = ObserverSetEntry<Parameters>(object: object, f: { f($0 as! T) })
         synchronized {
